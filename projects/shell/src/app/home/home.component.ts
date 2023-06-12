6e3318bf64fc47f239fd89991a47759a40d8d401
Torben Lottmann <torben.lottmann@lht.dlh.de>
2023-06-12 15:46:31 -0600

    feat: federated plugins

--- projects/shell/src/app/home/home.component.ts
+++ projects/shell/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { WalletLibService } from 'wallet-lib';
+import { PluginOptions } from '../plugins/plugins';
+import { LookupService } from '../plugins/lookup.service';
 
 @Component({
   selector: 'app-home',
@@ -8,12 +10,19 @@ import { WalletLibService } from 'wallet-lib';
 export class HomeComponent implements OnInit {
 
   balance = 0;
+  plugins: PluginOptions[] = [];
+  graphs: PluginOptions[] = [];
 
-  constructor(private walletService: WalletLibService) {
+  constructor(private walletService: WalletLibService, private lookupService: LookupService) {
     this.balance = this.walletService.balance;
   }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    this.plugins = await this.lookupService.lookup();
+  }
+
+  handleClick(graph: PluginOptions): void {
+    this.graphs.push(graph);
   }
 
 }
